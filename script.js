16336b6e771a3df92db646716868d6bf8a5ba06a
edongmin4894-png <edongmin4894@gmail.com>
2026-07-18 23:51:59 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function doPost(e) {
+  var sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
+  var data = JSON.parse(e.postData.contents);
+  
+  // 1. Base64로 인코딩된 이미지를 디코딩하여 구글 드라이브에 저장
+  var imageBlob = Utilities.newBlob(Utilities.base64Decode(data.imageFile), data.mimeType, data.fileName);
+  var folder = DriveApp.getFolderById("사진을_저장할_폴더_ID");
+  var file = folder.createFile(imageBlob);
+  var fileUrl = file.getUrl();
+  
+  // 2. 구글 시트에 데이터 추가
+  sheet.appendRow([new Date(), data.itemName, data.features, fileUrl]);
+  
+  return ContentService.createTextOutput("성공적으로 등록되었습니다.");
+}
